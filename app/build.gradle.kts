bfb72f6fd094aa7398db6361956d44f8f238c1e6
linuxct <linuxct@linuxct.space>
2026-07-11 21:41:28 +0200

    feat: fix wrong versionCode, bump versionName accordingly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "space.linuxct.teleforward"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
